951537d0297a729ebd9d5a29af263c2e6322d340
pamisa21 <arrampamisa21@gmail.com>
2025-05-19 14:48:13 +0800

    ay_sem

---
+++ sqlfolder/ay_sem.sql
@@ -0,0 +1,6 @@
+INSERT INTO `ay_sem` (`ay_id`, `ay_name`) VALUES ('23241ST', '2023-2024 1st Semester');
+INSERT INTO `ay_sem` (`ay_id`, `ay_name`) VALUES ('23242ND', '2023-2024 2nd Semester');
+INSERT INTO `ay_sem` (`ay_id`, `ay_name`) VALUES ('2324MY', '2023-2024 Midyear');
+INSERT INTO `ay_sem` (`ay_id`, `ay_name`) VALUES ('24251ST', '2024-2025 1st Semester');
+INSERT INTO `ay_sem` (`ay_id`, `ay_name`) VALUES ('24252ND', '2024-2025 2nd  Semester');
+INSERT INTO `ay_sem` (`ay_id`, `ay_name`) VALUES ('2425MY', '2024-2025 Midyear ');
